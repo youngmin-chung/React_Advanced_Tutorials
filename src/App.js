9c793b807918f044ba112b6ed4b8634f2209a00c
Youngmin Chung <mintmin01@gmail.com>
2021-05-22 00:03:59 -0400

    04_forms

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // set up the path here to run each tutorial example
-import Setup from './tutorial/03_conditional_rendering/03_show_hide'
+import Setup from './tutorial/04_forms/02_multiple_inputs'
 
 function App() {
   return (
---
+++ src/tutorial/04_forms/01_controlled_inputs.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+// JS
+// const input = document.getElementById('myText');
+// const inputValue = input.value
+// React
+// value, onChange
+
+const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState('')
+  const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(firstName, email)
+    if (firstName && email) {
+      console.log('submit the form')
+      const person = {
+        id: new Date().getTime().toString(),
+        firstName: firstName,
+        email: email,
+      }
+      console.log(person)
+      setPeople((people) => {
+        return [...people, person]
+      })
+      setFirstName('')
+      setEmail('')
+    } else {
+      console.log('empty values')
+    }
+  }
+  return (
+    <>
+      <article>
+        <form className='form' onSubmit={handleSubmit}>
+          <div className='form-control'>
+            <label htmlFor='firstName'>Name: </label>
+            <input
+              type='text'
+              id='firstName'
+              name='firstName'
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}></input>
+          </div>
+          <div className='form-control'>
+            <label htmlFor='email'>Email: </label>
+            <input
+              type='text'
+              id='email'
+              name='email'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}></input>
+          </div>
+          <button type='submit' onClick={handleSubmit}>
+            add person
+          </button>
+        </form>
+        {people.map((person, index) => {
+          const { id, firstName, email } = person
+          return (
+            <div key={id} className='item'>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          )
+        })}
+      </article>
+    </>
+  )
+}
+
+export default ControlledInputs
