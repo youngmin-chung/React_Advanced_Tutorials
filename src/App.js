89f3a94967d980b52195819b82f6af19913f9a1e
Youngmin Chung <mintmin01@gmail.com>
2021-06-04 18:49:38 -0400

    08_useContext

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // set up the path here to run each tutorial example
-import Setup from './tutorial/07_prop_drilling/01_prop_drilling'
+import Setup from './tutorial/08_useContext/01_context_api'
 
 function App() {
   return (
