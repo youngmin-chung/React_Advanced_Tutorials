a9c8016f196aedb1f3543b4e73e33bf8165438ce
Youngmin Chung <mintmin01@gmail.com>
2021-06-21 14:55:46 -0400

    11_react_router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react'
 
 // set up the path here to run each tutorial example
-import Setup from './tutorial/10_prop_types/index'
+import Setup from './tutorial/11_react_router/'
 
 function App() {
   return (
